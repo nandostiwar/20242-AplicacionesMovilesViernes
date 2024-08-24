8688dac0f9e9ddec4ad82b791fec13fc145e3452
MozartCuidadorDigital <45375029+nandostiwar@users.noreply.github.com>
2024-08-23 20:28:39 -0500

    subi los prooyectos de calculadora y horoscopo

--- calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
+++ calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
@@ -1,9 +1,11 @@
+
 const {add, subtract, multiply} = require('../operaciones/operaciones.js');
 
 function sumar(req, res){
     const {body} = req;
     const {number1, number2} = body;
     const result = add(number1, number2);
+    console.log("num1: " + String(number1) + " num2: " + String(number2) + " y la suma: " + String(result))
     res.json({
         resultado: result
     });
--- calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
+++ calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
@@ -10,6 +10,10 @@ function Calculadora(){
     function handleSubmit(e){
         e.preventDefault();
         const operacion = e.target.value;
+        if (operacion == "sumar"){setResultado(parseFloat(number1)+parseFloat(number2))}
+        if (operacion == "restar"){setResultado(parseFloat(number1)-parseFloat(number2))}
+        if (operacion == "multiplicar"){setResultado(parseFloat(number1)*parseFloat(number2))}
+
         fetch(`http://localhost:3500/v1/calculadora/${operacion}`, {
             method: 'POST',
             headers: {"Content-Type": "application/json"},
