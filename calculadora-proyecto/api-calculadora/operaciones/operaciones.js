93bbbaa86a4bd525e63f88771ba5bcb61b02aaa8
MateoMina <mateominaclavijo12@gmail.com>
2024-08-30 20:53:32 -0500

    Ejercicio 1: Calculadora

--- calculadora-proyecto/api-calculadora/operaciones/operaciones.js
+++ calculadora-proyecto/api-calculadora/operaciones/operaciones.js
@@ -23,8 +23,31 @@ function multiply(a, b){
     return number1 * number2;
 }
 
+function maxim(a, b){
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    return number1 < number2 ? number1 : number2;
+}
+
+function minim(a, b){
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    return number1 > number2 ? number1 : number2;
+}
+ 
+function promed(a, b){
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    p = ((number1+number2)/2)
+    return p;
+}
+
+
 module.exports = {
     add,
     subtract,
-    multiply
+    multiply,
+    maxim,
+    minim,
+    promed,
 }
--- calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
+++ calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
@@ -10,10 +10,6 @@ function Calculadora(){
     function handleSubmit(e){
         e.preventDefault();
         const operacion = e.target.value;
-        if (operacion == "sumar"){setResultado(parseFloat(number1)+parseFloat(number2))}
-        if (operacion == "restar"){setResultado(parseFloat(number1)-parseFloat(number2))}
-        if (operacion == "multiplicar"){setResultado(parseFloat(number1)*parseFloat(number2))}
-
         fetch(`http://localhost:3500/v1/calculadora/${operacion}`, {
             method: 'POST',
             headers: {"Content-Type": "application/json"},
@@ -36,6 +32,9 @@ function Calculadora(){
                 <input type="submit" className="btnEnviar" value="sumar" onClick={handleSubmit}/>
                 <input type="submit" className="btnEnviar" value="restar" onClick={handleSubmit}/>
                 <input type="submit" className="btnEnviar" value="multiplicar" onClick={handleSubmit}/>
+                <input type="submit" className="btnEnviar" value="mayor" onClick={handleSubmit}/>
+                <input type="submit" className="btnEnviar" value="menor" onClick={handleSubmit}/>
+                <input type="submit" className="btnEnviar" value="promedio" onClick={handleSubmit}/>                
             </form>
             <Resultado resultado={"El resultado es "+ resultado}/>
         </div>
