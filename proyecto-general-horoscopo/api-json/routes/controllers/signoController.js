805e5637ed1f3b7d537e65a83e1b3b1c8b888071
edwinbravo34 <edwinbravo2121@gmail.com>
2024-08-31 04:36:16 -0500

    modificacion en el horoscopo agregar genero

--- proyecto-general-horoscopo/api-json/routes/controllers/signoController.js
+++ proyecto-general-horoscopo/api-json/routes/controllers/signoController.js
@@ -1,24 +1,24 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-const getAllSignos = async (req, res)=>{
-    const signo = await fs.readFile(path.join(__dirname,'../../db/signos.json'));
+const getAllSignos = async (req, res) => {
+    const signo = await fs.readFile(path.join(__dirname, '../../db/signos.json'));
     const signosJson = JSON.parse(signo)
     res.json(signosJson);
 }
 
-const getOneSigno = async (req, res)=>{
+const getOneSigno = async (req, res) => {
     const oneSigno = req.params.signo;
-    const allSignos = await fs.readFile(path.join(__dirname,'../../db/signos.json'));
+    const allSignos = await fs.readFile(path.join(__dirname, '../../db/signos.json'));
     const objSignos = JSON.parse(allSignos);
     const result = objSignos[oneSigno];
     res.json(result)
 }
 
-const updateSigno = async (req, res)=>{
+const updateSigno = async (req, res) => {
     const signoEditar = req.params.signoEditar;
-    const {textoEditar} = req.body;
-    const allSignos = await fs.readFile(path.join(__dirname,'../../db/signos.json'));
+    const { textoEditar } = req.body;
+    const allSignos = await fs.readFile(path.join(__dirname, '../../db/signos.json'));
     const objSignos = JSON.parse(allSignos);
 
     const objUpdate = {
@@ -27,7 +27,7 @@ const updateSigno = async (req, res)=>{
     }
 
     // console.log(objUpdate);
-    await fs.writeFile(path.join(__dirname,'../../db/signos.json'), JSON.stringify(objUpdate, null, 2), {encoding: 'utf-8'})
+    await fs.writeFile(path.join(__dirname, '../../db/signos.json'), JSON.stringify(objUpdate, null, 2), { encoding: 'utf-8' })
 
     res.json({
         message: "Updated"
