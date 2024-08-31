98725070c5c7d58a0424585054802f9c840ed8de
Esteban-Lombar <esteban.castano01@unicatolica.edu.co>
2024-08-30 23:35:01 -0500

    Front srm

--- calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
+++ calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
@@ -7,35 +7,64 @@ function Calculadora(){
     const [number2, setNumber2] = useState('');
     const [resultado, setResultado] = useState('');
 
-    function handleSubmit(e){
-        e.preventDefault();
-        const operacion = e.target.value;
-        fetch(`http://localhost:3500/v1/calculadora/${operacion}`, {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({number1, number2})
-        })
-            .then(res =>res.json())
-            .then(responseData => {
-                setResultado(responseData.resultado)
-                // setResultado(responseData)
-                // console.log(resultado)
-            })
-    }
+    // function handleSubmit(e){
+    //     e.preventDefault();
+    //     const operacion = e.target.value;
+    //     fetch(`http://localhost:3500/v1/calculadora/${operacion}`, {
+    //         method: 'POST',
+    //         headers: {"Content-Type": "application/json"},
+    //         body: JSON.stringify({number1, number2})
+    //     })
+    //         .then(res =>res.json())
+    //         .then(responseData => {
+    //             setResultado(responseData.resultado)
+    //             // setResultado(responseData)
+    //             // console.log(resultado)
+    //         })
+    // }
 
     return (
         <div className="container">
             <h1 id="txtCalculadora">CALCULADORA</h1>
             <form>
-                <input type="text" className="number" onChange={(e)=>{setNumber1(e.target.value)}}/><br />
-                <input type="text" className="number" onChange={(e)=>{setNumber2(e.target.value)}}/><br />
-                <input type="submit" className="btnEnviar" value="sumar" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="restar" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="multiplicar" onClick={handleSubmit}/>
+                <input type="number" className="number" onChange={(e)=>{setNumber1(e.target.value)}}/><br />
+                <input type="number" className="number" onChange={(e)=>{setNumber2(e.target.value)}}/><br />
+                <input className="btnEnviar" value="sumar" onClick={operation}/>
+                <input className="btnEnviar" value="restar" onClick={operation}/>
+                <input  className="btnEnviar" value="multiplicar" onClick={operation}/>
             </form>
             <Resultado resultado={"El resultado es "+ resultado}/>
         </div>
     )
+
+    // function sumar(event) {
+    //     const sumaTotal = parseInt(number1) + parseInt(number2);
+    // }
+    // function resta(event){
+    //     const restaTotal = parseInt(number1) - parseInt(number2);
+    //     console.log(restaTotal)
+    // }
+
+    function operation(event) {
+
+        var total;
+
+        switch (event.target.value) {
+            case 'sumar':
+                total = parseInt(number1) + parseInt(number2);
+                break;
+            case 'restar':
+                total = parseInt(number1) - parseInt(number2);
+                break;
+            case 'multiplicar':
+                total = parseInt(number1) * parseInt(number2);
+                break;
+
+        }
+
+        console.log('Total', total)
+    }
+
 }
 
 export default Calculadora
