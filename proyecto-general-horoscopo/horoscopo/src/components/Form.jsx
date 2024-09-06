2ff1702c3744b7913dd2a94dd0d9c413b7eacbde
Rivera G Alejandro <alejandro110498@hotmail.com>
2024-09-05 23:42:56 -0500

    MOdificacion del proyecto del horoscopo

--- proyecto-general-horoscopo/horoscopo/src/components/Form.jsx
+++ proyecto-general-horoscopo/horoscopo/src/components/Form.jsx
@@ -9,10 +9,10 @@ function Form({callback}){
  
     const validateUser = (event)=>{
         event.preventDefault();
-        if(username === 'user' && password === 'user2023'){
+        if(username === 'user' && password === 'user123'){
             callback("user");
             goTo("/userHome");
-        }else if(username === 'admin' && password==='admin2023'){
+        }else if(username === 'admin' && password==='admin123'){
             callback("admin");
             goTo("/adminHome");
         }
