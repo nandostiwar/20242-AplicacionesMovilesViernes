6c0f357a7a3ca3a2ab26d51ae5d5e58f69623344
Rivera G Alejandro <alejandro110498@hotmail.com>
2024-09-05 22:23:56 -0500

    Calculadora Finalizado

--- calculadora-proyecto/api-calculadora/operaciones/operaciones.js
+++ calculadora-proyecto/api-calculadora/operaciones/operaciones.js
@@ -24,15 +24,29 @@ function multiply(a, b){
 }
 
 function isGreaterThan(a, b) {
-    let number1 = parseInt(a);
-    let number2 = parseInt(b);
-    return number1 > number2;
+    let number1 = parseFloat(a);
+    let number2 = parseFloat(b);
+    
+    if (number1 > number2) {
+        return number1; // number1 es mayor
+    } else if (number1 < number2) {
+        return number2; // number2 es meyor
+    } else {
+        return "Ambos números son iguales"; // number1 y number2 son iguales
+    }
 }
 
 function isLessThan(a, b) {
-    let number1 = parseInt(a);
-    let number2 = parseInt(b);
-    return number1 < number2;
+    let number1 = parseFloat(a);
+    let number2 = parseFloat(b);
+    
+    if (number1 < number2) {
+        return number1; // number1 es menor
+    } else if (number1 > number2) {
+        return number2; // number2 es menor
+    } else {
+        return "Ambos números son iguales"; // number1 y number2 son iguales
+    }
 }
 
 function average(a, b) {
