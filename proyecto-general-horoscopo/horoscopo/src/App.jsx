ccee23d95cb3bf374d1d745e8aad29cd33b74143
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-09-21 17:24:26 -0500

    Parcial 1 - Esteban Perdomo 405168

---
+++ proyecto-general-horoscopo/horoscopo/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import Form from './components/Form';
+import UserHome from './components/UserHome';
+import AdminHome from './components/AdminHome';
+import UpdatePassword from './components/updatePassword';
+import NewUser from './components/newuser';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { useState } from 'react';
+
+function App() {
+  const [user, setUser] = useState(null);
+  return (  
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Form callback={setUser}/>}></Route>
+        <Route path='/userHome' element={<UserHome user={user}/>}></Route>
+        <Route path='/adminHome' element={<AdminHome user={user}/>}></Route>
+        <Route path='/updatePassword' element={<UpdatePassword/>}></Route>
+        <Route path='/newuser' element={<NewUser/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ proyecto-general-horoscopo/horoscopo/src/components/newuser.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./styles/newuser.css"
+
+function NewUser() {
+  const [newUsername, setNewUsername] = useState("");
+  const [newUserPassword, setNewUserPassword] = useState("");
+  const [newUserPerfil, setNewUserPerfil] = useState("user");
+
+  const createNewUser = (event) => {
+    event.preventDefault();
+    fetch(`http://localhost:4000/v1/signos/newuser`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: newUsername,
+        password: newUserPassword,
+        perfil: newUserPerfil,
+      }),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        if (response.message === "Usuario creado exitosamente") {
+          alert("Usuario creado exitosamente");
+        } else {
+          alert("Error al crear usuario: " + response.message);
+        }
+      });
+  };
+
+  return (
+    <form onSubmit={createNewUser}>
+      <h4 className="txt">Nuevo Nombre de Usuario</h4>
+      <input type="text" className="entry" onChange={(e) => setNewUsername(e.target.value)} />
+      <br />
+      <h4 className="txt">Nueva Contraseña</h4>
+      <input type="password" className="entry" onChange={(e) => setNewUserPassword(e.target.value)} />
+      <br />
+      <h4 className="txt">Perfil</h4>
+      <select className="entry" onChange={(e) => setNewUserPerfil(e.target.value)}>
+        <option value="user">User</option>
+        <option value="admin">Admin</option>
+      </select>
+      <br />
+      <button className="boton-crear" type="submit">Crear Usuario</button>
+    </form>
+  );
+}
+
+export default NewUser;
